f9cd84c6eb2bba5068426efc1ddd2cb707e857fe
foton-1 <ovf0675@outlook.com>
2023-05-28 19:26:04 +0300

    add task-1
    зробив задачу 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,3 +4,45 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = createGaleryMarkup(galleryItems);
+galleryEl.style.listStyle = 'none';
+
+galleryEl.insertAdjacentHTML(`beforeend`, galleryMarkup);
+
+function createGaleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>`;
+    })
+    .join(``);
+}
+
+galleryEl.addEventListener(`click`, onGalleryElClick);
+
+function onGalleryElClick(event) {
+  if (!event.target.classList.contains(`gallery__image`)) {
+    return;
+  }
+  //   event.preventDefault();
+  console.log(event.target);
+}
+
+// Ініціалізація бібліотеки SimpleLightbox
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  /* options */
+});
