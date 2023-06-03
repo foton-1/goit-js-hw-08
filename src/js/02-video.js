fe8d30dbdba31024c5ef9d4f421bf53bce533ede
foton-1 <ovf0675@outlook.com>
2023-06-03 21:31:09 +0300

    дз 8.2 problem
    Не можу зробити 8.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
 
+const player = new Player('iframe', {
+  id: 'vimeo-player',
+});
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+/* 1-- Додана бібліотека Vimeo плеєра npm install @vimeo/player --
+   2-- */
