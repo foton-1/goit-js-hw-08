1aa39f5bc0ef3edaeaa4ee06b4b8a95240eb9d41
foton-1 <ovf0675@outlook.com>
2023-06-04 15:31:19 +0300

    task 8.2
    зроблено дз 8.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,45 @@
+// --//
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const player = new Player('iframe', {
-  id: 'vimeo-player',
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe, {
+  // id: ,
+  width: iframe.width,
 });
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
 
-/* 1-- Додана бібліотека Vimeo плеєра npm install @vimeo/player --
-   2-- */
+const onPlay = function ({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+  console.log(seconds);
+};
+player.on('timeupdate', throttle(onPlay, 2000));
+let actualTime = localStorage.getItem('videoplayer-current-time');
+console.log(actualTime);
+player
+  .setCurrentTime(actualTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+/* 1-- Додана бібліотека Vimeo плеєра: npm install @vimeo/player --
+   2-- Ініціалізація плеєра у файлі скрипта: import Player from '@vimeo/player'; const iframe = document.querySelector('#vimeo-player');
+   3-- Відстеження події timeupdate - оновлення часу відтворення: player.on('timeupdate', onPlay); 
+   4-- Запис в локальне сховище часу відтворення: localStorage.setItem('videoplayer-current-time', 'timeupdate.seconds');
+   5-- Відновлення відтворення зі збереженої позиції під час перезавантаження методом setCurrentTime()
+   6-- Налаштування, щоб час відтворення оновлювався у сховищі не частіше 1с за допомогою бібліотеки lodash.throttle : npm i -g npm; throttle
+*/
